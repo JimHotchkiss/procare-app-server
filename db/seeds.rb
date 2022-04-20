06261e32b2f5fc0cb6d19ce6014be6f0fbc59d75
Jimmy <jimmy.hotchkiss@gmail.com>
2022-04-20 16:24:45 -0700

    Updated seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ require 'faker'
 # Instruction.destroy_all
 def seed_instructions 
     30.times do |count|
-        instruction = Instruction.create(title: Faker::Name.unique.name)
+        instruction = Instruction.create(title: "Instruction: #{Faker::IDNumber.valid}")
     end 
     seed_repairs()
 end
@@ -18,7 +18,7 @@ end
 def seed_repairs
     Instruction.all.each_with_index do |element, index|
         7.times do |count|
-            repair = element.repairs.create(title: "Repair #{element.title} #{count + 1}", description: Faker::Lorem.paragraph(sentence_count: 2))
+            repair = element.repairs.create(title: "Repair: #{element.title} #{count + 1}", description: Faker::Lorem.paragraph(sentence_count: 2))
         end 
     end 
     seed_steps()
@@ -27,7 +27,7 @@ end
 def seed_steps 
     Repair.all.each_with_index do |element, index|
         5.times do |count|
-            step = element.steps.create(title: "#{element.title}.#{count + 1}", description: Faker::Lorem.paragraph(sentence_count: 2)) 
+            step = element.steps.create(title: "Step: #{element.title}.#{count + 1}", description: Faker::Lorem.paragraph(sentence_count: 2)) 
         end 
     end 
 end 
